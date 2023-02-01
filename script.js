4e50746fc1bbdee4e1542bb2491faba76811e1f9
stjepka <ivana0stjepanovic@gmail.com>
2023-02-01 17:36:14 +0100

    javascript

---
+++ script.js
@@ -0,0 +1,20 @@
+var firstName = "Ivana ";
+var lastName = "Stjepanović";
+fullName = firstName + lastName;
+console.log(fullName);
+
+
+for (let count = 1; count < 20; count++){
+    if (count % 2 === 0){
+        console.log("Broj " + count + " je paran.")
+    }
+}
+
+
+var z;
+var y;
+var x;
+
+if(z === 2){
+    x = z;
+} else {x = 5};
