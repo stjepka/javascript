bb081ee21da8ba6bd3e776e19d110f345d6fcc12
stjepka <ivana0stjepanovic@gmail.com>
2023-02-21 21:36:42 +0100

    lottery

--- Lottery-app/script.js
+++ Lottery-app/script.js
@@ -1,7 +1,28 @@
 import Lottery from "./modules/lottery.js";
 import { politicians, folk } from "./data/data.js";
 
-const lottery = new Lottery(politicians); //istanciranje objekta, povezivanje politiciansa s ostalim
-lottery.startDrawing();
+const buttonStartLotteryEl = document.querySelector('.button-start-lottery');
+const lottteryResultsEl = document.querySelector(".lottery-results");
+const winningCombinationEl = document.querySelector(".winning-combination");
+const winningMessageEl = document.querySelector(".winners-message");
+const winnersEl = document.querySelector(".winners");
 
-console.log(lottery);
+const lottery = new Lottery(politicians); //istanciranje objekta, povezivanje politiciansa s ostalim
+
+buttonStartLotteryEl.addEventListener("click", function () {
+    buttonStartLotteryEl.disabled = true;
+    buttonStartLotteryEl.innerText = "Lottery drawing in progress...."
+    lottery
+    .startDrawing()
+    .then((result) => {
+        winningCombinationEl.innerText = `Winning combo was: ˘${result.winningCombination}`;
+    })
+    .catch((result) => {
+     
+    })
+
+    .finally(() => {
+        buttonStartLotteryEl.disabled = false;
+        buttonStartLotteryEl.innerText = "Start lottery drawing"
+    });
+});
